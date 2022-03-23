05e87403de2ba0bce878d0bf86c38b2e585b4c35
GhostyO17 <oscar.castro9617@gmail.com>
2022-03-22 22:29:28 -0500

    adds _printf.c and main.h

--- _printf.c
+++ _printf.c
@@ -1,1 +1,48 @@
-int _printf
+#include "main.h"
+/**
+*
+*
+*
+*/
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int i = 0, bcount = 0;
+    char *pstr, charc, charp;
+    va_start(args, format);
+
+
+    if (format == NULL)
+    {
+       return (-1); 
+    }
+
+    for (; format[i] != '\0'; i++, bcount++)
+    {
+        if (format[i] == '%')
+        {
+            charp = format[i + 1];
+            switch (charp)
+            {
+            case 'c':
+                charc = va_arg(args, int);
+                _putchar(charc);
+                i += 2, bcount++;
+                break;
+            case 's':
+                pstr = va_arg(args, char*);
+                if (pstr == NULL)
+                {
+                    write(1, "(null)", 6);
+                    i += 2, bcount += 6;
+                    break;
+                }
+                write(1, pstr, )
+                break;    
+            default:
+                break;
+            }
+        }
+    }
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <string.h>
+
+/* PRINTF PROTOTYPE */
+int _printf(const char *format, ...);
+
+/* OTHER */
+int _putchar(char c);
+
+
+#endif
