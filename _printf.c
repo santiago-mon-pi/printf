efaa035c74af53b2e19d10a318c1ca70dee7d15b
GhostyO17 <oscar.castro9617@gmail.com>
2022-03-22 23:02:46 -0500

    adds complete printf function

--- _printf.c
+++ _printf.c
@@ -38,11 +38,33 @@ int _printf(const char *format, ...)
                     i += 2, bcount += 6;
                     break;
                 }
-                write(1, pstr, )
+                write(1, pstr, _len(pstr));
+                i += 2, bcount += _len(pstr);
                 break;    
-            default:
+            case '%':
+                _putchar('%');
+                i += 2, bcount++;
                 break;
-            }
-        }
-    }
+            default:
+                if (charp == NULL)
+                {
+                    return (-1);
+                }
+                else if ((charp < 65) || (charp > 90) 
+                            && (charp < 97) || (charp < 123))
+                {
+                    _putchar('%');
+                    bcount += 1, i += 1;
+                }
+                i += 2;
+                break;
+            };
+        };
+        if (charp != '\0' && charp != '%')
+        {
+            _putchar(format[i]);
+        };
+    };
+    va_end(args);
+    return (bcount);
 }
